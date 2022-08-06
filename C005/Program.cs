f13f4957513fed35a241e8f2996aa2fe8893f3a1
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-06 20:21:05 +0500

    005

---
+++ C005/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задать рандомное вещественное чисело
+double test = new Random().NextDouble();
+System.Console.WriteLine(test);
+test*=100;
+System.Console.WriteLine(Math.Round(test,3));
+// 5 строка округление до 3 знаков после запятой
