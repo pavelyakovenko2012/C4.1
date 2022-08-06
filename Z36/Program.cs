988a3f28d6ef66a3557d1b26b92e4981dfd0275d
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-06 17:21:28 +0500

    Z36

---
+++ Z36/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write($"Введи кол-во элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine());
+int RandomNumbers(int numberElements, int min, int max)
+{
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+    for (int i = 0; i <randomNumbers.Length; i++ )
+    {
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+    return sumElements;
+}
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+Console.WriteLine($"\nСумма нечётных элементов: {randomNumbers}");
