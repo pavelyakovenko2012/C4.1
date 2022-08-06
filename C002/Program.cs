4ff67861f7700ad3bde3ed0ec20565e8a5fe4013
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-06 17:19:57 +0500

    002

---
+++ C002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+int[] array = {-2,-1,1,2};
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]*=(-1);
+    // или array[i]=-array[i]
+    System.Console.Write(array[i] + "\n");
+    // System.Console.Write(array[i] + "\t"); t-Tab,
+    // System.Console.WriteLine() = System.Console.Write("\n"); n-new
+}
+System.Console.Write("\n");
+// System.Console.Write()
+System.Console.WriteLine("Тест");
