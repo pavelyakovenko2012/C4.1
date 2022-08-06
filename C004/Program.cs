d326f10ea105950722fc0ca9dd4b64287ae79f4c
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-06 17:20:53 +0500

    004

---
+++ C004/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Решение---1:
+// int[] GetArray (int size, int minRandom, int maxRandom)
+// {
+//     int[] result = new int [size];
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = new Random().Next(minRandom, maxRandom+1);
+//     }
+//     return result;
+// }
+// int[] array = GetArray(123,0,99);
+// System.Console.WriteLine($"Результат: [{String.Join(";", array)}]");
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if(array[i]>10 && array[i]<99) count++;
+// }
+// System.Console.WriteLine($"Результат: {count}");
+
+// Решение---2:
+int[] array = new int[123];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(0,999);
+    System.Console.Write(array[i]+"\t");
+    if (array[i]>=10&&array[i]<=99)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"Колличество чисел в диапозоне [10,99] = {count}");
